35d66946d6d351dc1d4a5855e02d8e78e021e4d7
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-05-05 18:56:50 +0300

    update README to reflect iOS version change and improve installation instructions; remove unnecessary dependency from build.gradle.kts

--- signInWithGoogle/build.gradle.kts
+++ signInWithGoogle/build.gradle.kts
@@ -192,7 +192,6 @@ kotlin {
                 implementation(libs.androidx.credentials)
                 implementation(libs.androidx.credentials.play.services.auth)
                 implementation(libs.googleid)
-                implementation(libs.androidx.startup.runtime)
             }
         }
 

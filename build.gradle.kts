a62154679d0bc7ac3902e9d61c4956703498a029
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-05-05 18:57:35 +0300

    add maven publish plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.maven.publish) apply false
 }
